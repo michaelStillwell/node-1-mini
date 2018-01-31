05c12ff96ebb82615a6ce8acb5555331a4f609dd
Michael <michael.stillwell@hotmail.com>
2018-01-30 18:21:45 -0600

    Finished

---
+++ server/controllers/books_controller.js
@@ -0,0 +1,35 @@
+const books = [];
+const id = 0;
+
+module.exports = {
+    create: ( req, res ) => {
+        const { title, author } = req.body;
+        books.push( {id, title, author} );
+        id++;
+        res.status(200).send(books);
+    },
+
+    read: ( req, res ) => {
+        res.status(200).send(books);
+    },
+
+    update: ( req, res ) => {
+        const updateID = req.params.id;
+        let index = books.findIndex( book => book.id == updateID );
+
+        books[index] = {
+            id: books[index].id,
+            title: req.body.title || books[index].title,
+            author: req.body.auhor || books[index].author,
+        };
+
+        res.status(200).send(books);
+    },
+
+    delete: ( req, res ) => {
+        const deleteID = req.params.id;
+        bookID = books.findIndex( book => book.id == deleteID );
+        books.splice(bookID, 1);
+        res.status(200).send(books);
+    }
+};
